22cfa0f234a92a99d308f9e1a6a68fb52cfbbe4f
Giovanni Bussi <giovanni.bussi@gmail.com>
2014-12-03 10:17:10 +0100

    Added script to push documentation
    (currently pushing on a test repository)

---
+++ src/maketools/pushdoc.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+set -e
+set -x
+
+hash=$( git log -1 --format="%h")
+
+root="$PWD"
+mkdir -p tmp/$$
+cd tmp/$$
+
+# grab doc
+cp -R $root/*doc .
+
+# delete gitignore files
+rm */.gitignore
+
+# This file should be here, it is needed otherwise
+# files beginning with _ are skipped
+touch .nojekyll
+
+cat > index.html << EOF
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title>Page Auto Redirect</title>
+<meta http-equiv="refresh" content ="0; user-doc/html/index.html">
+</head>
+<body>
+This is an auto redirect page.
+</body>
+</html>
+EOF
+
+git init
+git checkout -b gh-pages
+git remote add doc git@github.com:GiovanniBussi/test-plumed-doc.git
+git add --all .
+git commit -m "Update to plumed/plumed2@$hash"
+git push -f doc gh-pages
